af7707d7cac2607a9d1509578a4b377324d60087
yisheng-zhou <zhouys93@hotmail.com>
2024-10-10 13:54:10 -0700

    Add percentage log; Add System bean log

--- memq/src/main/java/com/pinterest/memq/core/rpc/CongestionControlHandler.java
+++ memq/src/main/java/com/pinterest/memq/core/rpc/CongestionControlHandler.java
@@ -1,5 +1,6 @@
 package com.pinterest.memq.core.rpc;
 
+import com.sun.management.OperatingSystemMXBean;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.ChannelDuplexHandler;
@@ -8,13 +9,16 @@ import io.netty.channel.ChannelPromise;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.management.ManagementFactory;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public final class CongestionControlHandler extends ChannelDuplexHandler {
 
   private final Logger logger = LoggerFactory.getLogger(CongestionControlHandler.class);
+  private static final Random random = new Random();
 
   public CongestionControlHandler() {
   }
@@ -37,25 +41,46 @@ public final class CongestionControlHandler extends ChannelDuplexHandler {
 
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    int randomValue = random.nextInt(100);
+    if (randomValue < 90) {
+      // Drop log messages
+      return;
+    }
     // Check runtime memory usage.
     Runtime runtime = Runtime.getRuntime();
     long memoryMax = runtime.maxMemory();
     long memoryUsed = runtime.totalMemory() - runtime.freeMemory();
-    double memoryUsedPercentDouble = (double) memoryUsed / memoryMax * 100;
-    long memoryUsedPercent = Math.round(memoryUsedPercentDouble);
+    double memoryUsedPercent = (double) memoryUsed / memoryMax * 100;
+    // Check netty memory usage.
     long unpooledMemoryUsed = UnpooledByteBufAllocator.DEFAULT.metric().usedHeapMemory();
     long pooledMemoryUsed = PooledByteBufAllocator.DEFAULT.metric().usedHeapMemory();
     long unpooledHeapMemoryUsed = UnpooledByteBufAllocator.DEFAULT.metric().usedDirectMemory();
     long pooledHeapMemoryUsed = PooledByteBufAllocator.DEFAULT.metric().usedDirectMemory();
-    List<Long> usageList = new ArrayList<Long>() {
+    // Check system memory usage.
+    OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(
+        OperatingSystemMXBean.class);
+    long physicalMemorySize = osBean.getTotalPhysicalMemorySize();
+    long freePhysicalMemorySize = osBean.getFreePhysicalMemorySize();
+    long usedPhysicalMemorySize = physicalMemorySize - freePhysicalMemorySize;
+    // Check CPU usage
+    double processCpuLoad = osBean.getProcessCpuLoad() * 100;
+    double systemCpuLoad = osBean.getSystemCpuLoad() * 100;
+
+    List<String> usageList = new ArrayList<String>() {
       {
-        add(memoryMax);
-        add(memoryUsed);
-        add(memoryUsedPercent);
-        add(unpooledMemoryUsed);
-        add(pooledMemoryUsed);
-        add(unpooledHeapMemoryUsed);
-        add(pooledHeapMemoryUsed);
+        add(String.valueOf(System.currentTimeMillis()));
+        add(String.valueOf(memoryMax));
+        add(String.valueOf(memoryUsed));
+        add(String.valueOf(memoryUsedPercent));
+        add(String.valueOf(unpooledMemoryUsed));
+        add(String.valueOf(pooledMemoryUsed));
+        add(String.valueOf(unpooledHeapMemoryUsed));
+        add(String.valueOf(pooledHeapMemoryUsed));
+        add(String.valueOf(physicalMemorySize));
+        add(String.valueOf(freePhysicalMemorySize));
+        add(String.valueOf(usedPhysicalMemorySize));
+        add(String.valueOf(processCpuLoad));
+        add(String.valueOf(systemCpuLoad));
       }
     };
     logger.info("[TEST_METRICS] " + usageList.toString());
