01ec3a94316c1d4d2cbcae84ce9eb91bb0dd9d1e
Yisheng Zhou <yishengzhou@pinterest.com>
2024-11-07 11:00:49 -0800

    6MB test

--- memq/src/main/java/com/pinterest/memq/core/rpc/MemqNettyServer.java
+++ memq/src/main/java/com/pinterest/memq/core/rpc/MemqNettyServer.java
@@ -98,7 +98,7 @@ public class MemqNettyServer {
     Authorizer authorizer = enableAuthenticationAuthorizationAuditing(configuration);
 
     // TODO: Load from memq config 
-    long readLimit = 1024 * 1024 * 3; // 3MB per second per broker
+    long readLimit = 1024 * 1024 * 6; // 6MB per second per broker
     long checkIntervalMs = 10000;
     boolean enableTrafficShaping = true;
 
