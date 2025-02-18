89aab0fb42e8a121003072bfe1bfce0d63fd7206
Jeff Xiang <jxiang@pinterest.com>
2025-02-18 17:15:26 -0500

    Call payload.release() in Request.Dispatch

--- memq-client/src/main/java/com/pinterest/memq/client/producer2/Request.java
+++ memq-client/src/main/java/com/pinterest/memq/client/producer2/Request.java
@@ -416,9 +416,11 @@ public class Request {
         }
         logger.error("Failed to send request " + clientRequestId, resultException);
         resolve(resultException);
+        payload.release();
       } else {
         logger.error("Failed to send request " + clientRequestId, throwable);
         resolve(throwable);
+        payload.release();
       }
     }
 
