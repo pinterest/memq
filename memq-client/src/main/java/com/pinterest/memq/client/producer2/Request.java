26ad15fd4122fd62239dc5151c8085a76b65bb1b
Jeff Xiang <jxiang@pinterest.com>
2025-02-21 10:51:12 -0500

    Call DIRECT.buffer(initCapacity, maxCapacity) instead of DIRECT.buffer(initCapacity)

--- memq-client/src/main/java/com/pinterest/memq/client/producer2/Request.java
+++ memq-client/src/main/java/com/pinterest/memq/client/producer2/Request.java
@@ -120,7 +120,7 @@ public class Request {
     this.compression = compression;
     this.metricRegistry = metricRegistry;
     int bufferCapacity = getByteBufCapacity(maxRequestSize, compression);
-    this.byteBuf = PooledByteBufAllocator.DEFAULT.buffer(bufferCapacity);
+    this.byteBuf = PooledByteBufAllocator.DEFAULT.buffer(bufferCapacity, bufferCapacity);
     try {
       initializeOutputStream();
     } catch (IOException ioe) {
