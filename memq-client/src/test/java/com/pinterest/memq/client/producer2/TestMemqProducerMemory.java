77a9b4f9a3e56bbebc0a705e7b41d334fea56ba9
Jeff Xiang <jxiang@pinterest.com>
2025-07-30 19:40:21 -0400

    Add another test

--- memq-client/src/test/java/com/pinterest/memq/client/producer2/TestMemqProducerMemory.java
+++ memq-client/src/test/java/com/pinterest/memq/client/producer2/TestMemqProducerMemory.java
@@ -17,36 +17,26 @@ package com.pinterest.memq.client.producer2;
 
 import com.codahale.metrics.MetricRegistry;
 import com.pinterest.memq.client.commons.Compression;
-import com.pinterest.memq.client.commons.MemqMessageHeader;
 import com.pinterest.memq.client.commons.serde.ByteArraySerializer;
 import com.pinterest.memq.client.commons2.MemoryAllocationException;
 import com.pinterest.memq.client.commons2.MockMemqServer;
-import com.pinterest.memq.client.commons2.network.netty.ClientChannelInitializer;
 import com.pinterest.memq.client.producer.MemqWriteResult;
 import com.pinterest.memq.commons.protocol.RequestPacket;
 import com.pinterest.memq.commons.protocol.RequestType;
 import com.pinterest.memq.commons.protocol.ResponseCodes;
 import com.pinterest.memq.commons.protocol.ResponsePacket;
 import com.pinterest.memq.commons.protocol.WriteResponsePacket;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.internal.OutOfDirectMemoryError;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -58,6 +48,87 @@ import static org.junit.Assert.fail;
 public class TestMemqProducerMemory extends TestMemqProducerBase {
 
   private static final Logger logger = Logger.getLogger(TestMemqProducerMemory.class.getName());
+
+  /**
+   * This test simulates a local write scenario where the producer writes 2 messages to a local broker which has
+   * TrafficShapingHandler enabled with a low read limit. This should result in the first write succeeding quickly, while
+   * the second write blocks until the first write is acknowledged by the broker.
+   *
+   * The second write blocks because we configured the producer to create one request per message, so the second write
+   * must try to create a new Request. Upon attempting to create a new request, it would have realized that the first write
+   * request is still in-flight, and that it cannot create a new request until the first one is acknowledged because the max
+   * inflight requests memory bytes is set to the size of one request. Therefore, the producer can only accommodate one
+   * inflight request at a time.
+   *
+   * We set the maxBlockMs config to a high enough value so that there is sufficient time for the broker to acknowledge the first write
+   * before the second write times out. The second write should block for a considerable time, but eventually succeed with a much higher ack latency.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testLocalBlockedWrites() throws Exception {
+    int messageValueBytes = 8192; // 8 kB
+    int maxPayloadBytes = messageValueBytes + 4096; // 1 message each request due to some additional header overhead
+    int maxInflightRequests = 999999999;  // effectively unlimited for this test
+    int maxInflightRequestsMemoryBytes = maxPayloadBytes; // it can only accommodate one inflight request
+
+    MockMemqServer mockMemqServer = getMockMemqServerWithTrafficShaping(10, 1000); // it should be able to accept 1 write at a time
+    mockMemqServer.start();
+
+    MemqProducer.Builder<byte[], byte[]> builder = new MemqProducer.Builder<>();
+    Properties networkProperties = new Properties();
+    byte[] sampleValue = new byte[8192];
+    builder
+            .cluster("prototype")
+            .topic("test")
+            .bootstrapServers(LOCALHOST_STRING + ":" + 20000)
+            .keySerializer(new ByteArraySerializer())
+            .valueSerializer(new ByteArraySerializer())
+            .maxPayloadBytes(maxPayloadBytes)
+            .compression(Compression.NONE)
+            .maxInflightRequests(maxInflightRequests)
+            .maxBlockMs(5000) // enough time for broker to ack the first write
+            .maxInflightRequestsMemoryBytes(maxInflightRequestsMemoryBytes)
+            .sendRequestTimeout(60 * 1000)  // we shouldn't hit this
+            .networkProperties(networkProperties)
+            .metricRegistry(new MetricRegistry());
+
+    MemqProducer<byte[], byte[]> producer = builder.build();
+
+    // r0 write should succeed almost immediately
+    Future<MemqWriteResult> r0 = producer.write(
+            null,
+            sampleValue
+    );
+    assertTrue(producer.getInflightMemoryAvailablePermits() < maxPayloadBytes); // we should not have space for another request
+
+    // r1 write should block until r0's acknowledgement is received
+    long startTime = System.currentTimeMillis();
+    Future<MemqWriteResult> r1 = producer.write(
+            null,
+            sampleValue
+    );
+    long r1WriteElapsedTime = System.currentTimeMillis() - startTime;
+    logger.log(Level.INFO, "Elapsed time for r1 write: " + r1WriteElapsedTime + " ms");
+
+    assertTrue(r1WriteElapsedTime > 1000); // r1 should have blocked for a considerable time but eventually succeed
+
+    try {
+      MemqWriteResult r0WriteResult = r0.get();
+      logger.log(Level.INFO,"r0 ack latency: " + r0WriteResult.getAckLatency() + " ms");
+
+      MemqWriteResult r1WriteResult = r1.get();
+      logger.log(Level.INFO,"r1 ack latency: " + r1WriteResult.getAckLatency() + " ms");
+
+      assertTrue(r1WriteResult.getAckLatency() > r0WriteResult.getAckLatency()); // r1 should have a larger ack latency than r0
+      assertTrue(r0WriteResult.getAckLatency() < r1WriteElapsedTime && (double) r0WriteResult.getAckLatency() / r1WriteElapsedTime > 0.9); // r1 should have blocked for around the same amount of time it took for r0's ack
+    } catch (Exception e) {
+      fail("Both writes should have succeeded");
+    }
+
+    producer.close();
+    mockMemqServer.stop();
+  }
 
   /**
    * This test simulates a scenario where the producer is trying to write
@@ -153,16 +224,18 @@ public class TestMemqProducerMemory extends TestMemqProducerBase {
     logger.log(Level.INFO, "Ack latencies: " + ackLatencies);
     assertTrue(ackLatencies.get(ackLatencies.size() - 1) > ackLatencies.get(0)); // last ack latency should be greater than the first one
     assertTrue(ackLatencies.get(ackLatencies.size() - 1) / ackLatencies.get(0) > 20); // last ack latency should be at least 20x the first one
+
+    producer.close();
   }
 
   // Run this first before running the testDirectMemoryAllocationFailureOnWrite
   public static void main(String[] args) throws Exception {
     int readLimit = 1024 * 512;  // this is specifically tuned to 512 kB so that testDirectMemoryAllocationFailureOnWrite will hit MemoryAllocationException but the server can still eventually handle the writes
-    runMockMemqServerWithTrafficShapingHandler(readLimit);
-
+    MockMemqServer mockServer = getMockMemqServerWithTrafficShaping(readLimit, 10);
+    mockServer.start();
   }
 
-  private static void runMockMemqServerWithTrafficShapingHandler(int readLimit) throws Exception {
+  private static MockMemqServer getMockMemqServerWithTrafficShaping(int readLimit, int checkInterval) {
     Map<RequestType, BiConsumer<ChannelHandlerContext, RequestPacket>> map = new HashMap<>();
 
     short port = (short) 20000;
@@ -173,8 +246,8 @@ public class TestMemqProducerMemory extends TestMemqProducerBase {
               req.getClientRequestId(), req.getRequestType(), ResponseCodes.OK, new WriteResponsePacket());
       ctx.writeAndFlush(resp);
     });
-    MockMemqServer mockServer = new MockMemqServer(port, map, false, true, readLimit, 10);
-    mockServer.start();
+    MockMemqServer mockServer = new MockMemqServer(port, map, false, true, readLimit, checkInterval);
+    return mockServer;
   }
 }
 
